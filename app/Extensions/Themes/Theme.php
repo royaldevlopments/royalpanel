abc7754be9a06e936593202608729da709d8eae1
root <root@ip-172-31-5-36.ap-south-1.compute.internal>
2026-06-10 00:39:50 +0000

    Fix admin CSS: clean Theme class tags, symlink vendor assets

--- app/Extensions/Themes/Theme.php
+++ app/Extensions/Themes/Theme.php
@@ -2,34 +2,20 @@
 
 namespace Pterodactyl\Extensions\Themes;
 
-use Pterodactyl\Services\Helpers\AssetHashService;
-
 class Theme
 {
-    protected AssetHashService $assets;
-
-    public function __construct()
-    {
-        $this->assets = app(AssetHashService::class);
-    }
-
     public function css(string $resource): string
     {
-        return $this->assets->css($this->processCacheVersion($resource));
+        return '<link rel="stylesheet" href="' . $this->url($resource) . '">';
     }
 
     public function js(string $resource): string
     {
-        return $this->assets->js($this->processCacheVersion($resource));
+        return '<script src="' . $this->url($resource) . '"></script>';
     }
 
     public function url(string $resource): string
     {
-        return $this->assets->url($this->processCacheVersion($resource));
-    }
-
-    protected function processCacheVersion(string $resource): string
-    {
-        return str_replace('{cache-version}', time(), $resource);
+        return url(str_replace('{cache-version}', time(), $resource));
     }
 }
