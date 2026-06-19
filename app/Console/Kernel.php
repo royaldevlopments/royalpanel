9e4bdc3aa5ee6f0e158648042d1ce8a91048a24a
root <root@ip-172-31-5-36.ap-south-1.compute.internal>
2026-06-19 13:35:48 +0000

    Fix Kernel.php schedule() syntax from Blueprint installer

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,11 @@ use RoyalPanel\Console\Commands\Schedule\ProcessRunnableCommand;
 use RoyalPanel\Console\Commands\Maintenance\PruneOrphanedBackupsCommand;
 use RoyalPanel\Console\Commands\Maintenance\CleanServiceBackupFilesCommand;
 
+// Import Blueprint schedules, telemetry and library
+use RoyalPanel\Services\Telemetry\RegisterBlueprintTelemetry;
+use RoyalPanel\BlueprintFramework\GetExtensionSchedules;
+use RoyalPanel\BlueprintFramework\Libraries\ExtensionLibrary\Console\BlueprintConsoleLibrary as BlueprintExtensionLibrary;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -28,6 +33,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('bp:telemetry')->hourly();
+
+        $schedule->command('bp:version:cache')->dailyAt('04:00');
+
+        $schedule->command('bp:cache')->everyMinute();
         // https://laravel.com/docs/10.x/upgrade#redis-cache-tags
         $schedule->command('cache:prune-stale-tags')->hourly();
 
@@ -44,9 +54,29 @@ class Kernel extends ConsoleKernel
             $schedule->command(PruneCommand::class, ['--model' => [ActivityLog::class]])->daily();
         }
 
+        // RoyalPanel telemetry
         if (config('royalpanel.telemetry.enabled')) {
             $this->registerTelemetry($schedule);
         }
+
+        // ============================
+        //    BLUEPRINT SCHEDULES
+        // ============================
+
+        // Blueprint telemetry
+        $blueprint = app()->make(BlueprintExtensionLibrary::class);
+        if ($blueprint->dbGet('blueprint', 'flags:telemetry_enabled', 0)) {
+            $registerBlueprintTelemetry = app()->make(RegisterBlueprintTelemetry::class);
+            $registerBlueprintTelemetry->register($schedule);
+        }
+
+        // Blueprint-related utilities
+        $randTime = str_pad(rand(0, 23), 2, '0', STR_PAD_LEFT) . ':' . str_pad(rand(0, 59), 2, '0', STR_PAD_LEFT);
+        $schedule->command('bp:version:cache')->dailyAt($randTime);
+        $schedule->command('bp:meta')->dailyAt($randTime);
+
+        // Blueprint extension schedules
+        GetExtensionSchedules::schedules($schedule);
     }
 
     /**
